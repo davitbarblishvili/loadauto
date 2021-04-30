b07ee10e8eab994778f1bc994a73606f12014144
Davit Barblishvili <db3230@columbia.edu>
2021-04-29 23:30:00 -0400

    added refreshing, and table iteration

--- acv_test.py
+++ acv_test.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+import json
 
 class acv(unittest.TestCase):
-    """A sample test class to show how page object works"""
+    url = ""
 
     def setUp(self):
         self.webdriver = webdriver.Chrome(executable_path=r"/Users/davitbarblishvili/Desktop/acv/chromedriver")
@@ -23,14 +24,51 @@ class acv(unittest.TestCase):
         password.send_keys(Keys.ENTER)
 
        
-
     def tearDown(self):
         self.webdriver.close()
+        
+    def getUrl(self):
+        time.sleep(2)
+        self.webdriver.find_element_by_xpath("//input[@name='Submit3']").click()
+    
+    def refreshPage(self):
+        time.sleep(1)
+        self.webdriver.find_element_by_xpath("//div[@class='arial14']/a[1]").click()
+
+    def iterateTr(self):
+        time.sleep(1)
+        acv.refreshPage()
+        keys = ["order_id","date","vehicle","inop?","P_address","P_city","P_state","P_zip","Daddress",
+        "D_city","D_state","D_zip","Distance","Mayuti"]
+        load_dict = {}
+        self.webdriver.find_element_by_xpath("//select[@name='perpage']/option[text()='All']").click()
+        table = self.webdriver.find_element_by_xpath("//table[2]")
+        test = 0
+        for row in table.find_elements_by_xpath(".//tr[@class='rowheight']"):
+            idx = 0
+            temp_dict = {}
+            for td in row.find_elements_by_xpath(".//td[@class='arial14']"):
+                if td.text and idx >= 1:
+                    temp_dict[keys[idx]] = td.text
+                    idx += 1
+                   
+                if td.text and idx == 0:
+                    load_dict[td.text] = temp_dict
+                    idx += 1
+                
+
+        with open('loads.json', 'w') as fp:
+            json.dump(load_dict, fp)
+
+
+
 
 if __name__ == "__main__":
     acv = acv() 
     acv.setUp()
     acv.login()
+    acv.iterateTr()
+   
     
 
 
