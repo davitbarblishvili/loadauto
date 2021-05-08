13e6764db891d4f2ef76dae46b38b8acff232cc7
Davit Barblishvili <db3230@columbia.edu>
2021-05-08 01:33:16 -0400

    jquery to flask pass completed

--- test.py
+++ test.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def output():
 	# serve index template
-	return render_template('acvlanding.html', name='Joe')
+	return render_template('acvlanding.html')
+
 @app.route('/receiver', methods = ['POST'])
 def worker():
 	# read json + reply
 	data = request.get_json()
-	result = ''
-	for item in data:
-		# loop over every row
-		result += str(item['make']) + '
-'
-	return result
+	
+	pick_up = str(data[0]['pu'])
+	deliv = str(data[1]['del'])
+	print(pick_up)
+	print(deliv)
+	return 'OK'
 if __name__ == '__main__':
 	# run!
 	app.run()
