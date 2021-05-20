8e9a519493dc4ab99695e2f4f8e970171d5f0586
Davit Barblishvili <db3230@columbia.edu>
2021-05-19 23:22:01 -0400

    updated app

--- app.py
+++ app.py
@@ -70,11 +70,10 @@ class acv(unittest.TestCase):
 
     def login(self):
                
-        time.sleep(2)
+      #  time.sleep(2)
         username = self.webdriver.find_element_by_xpath("//input[@name='email']")
         self.webdriver.execute_script("arguments[0].click();", username)
         username.send_keys("righttimenyc@yahoo.com")
-
         password = self.webdriver.find_element_by_xpath("//input[@name='password']")
         self.webdriver.execute_script("arguments[0].click();", password)
         password.send_keys("Tsikara95#")
