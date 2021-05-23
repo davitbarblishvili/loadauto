f5add77d21aa87fe64e4cc92c795232d153d4179
Davit Barblishvili <db3230@columbia.edu>
2021-05-22 20:58:19 -0400

    test

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def count_words_at_url(pick_up_state,dollar, minTotalDollar,dist,condition):
-    message = str(pick_up_state) + str(dollar) + str(minTotalDollar) + str(dist) + str(condition)
+    message = str(pick_up_state) + " " + str(dollar)  + " " + str(minTotalDollar) + " " + str(dist) + " " + str(condition)
     print(message)
     return message
     
