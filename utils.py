a9ba4771cce3e9d112762513b9b6824838c1d436
Davit Barblishvili <db3230@columbia.edu>
2021-09-07 23:29:07 -0400

    testing

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import searcher
 from rq import get_current_job
 
-def no_filter_search(pick_up_state,delivery_state):
+
+def no_filter_search(pick_up_state, delivery_state):
     acv = searcher.acv()
-    acv.initDatabase()
     acv.setUp()
     acv.login()
 
@@ -11,27 +11,25 @@ def no_filter_search(pick_up_state,delivery_state):
     if len(delivery_state) == 1 and delivery_state[0] == '':
         acv.one_way_no_filter(pick_up_state)
         return 'OK'
-    
-    acv.two_way_no_filter(pick_up_state,delivery_state)
+
+    acv.two_way_no_filter(pick_up_state, delivery_state)
     return 'OK'
-    
 
 
-def one_state_search(pick_up_state,dollar, minTotalDollar,dist,condition):  
+def one_state_search(pick_up_state, dollar, minTotalDollar, dist, condition):
     print("inside the function one way")
     acv = searcher.acv()
-    acv.initDatabase()
     acv.setUp()
     acv.login()
-    acv.one_way(pick_up_state,dollar,minTotalDollar,dist,condition)
+    acv.one_way(pick_up_state, dollar, minTotalDollar, dist, condition)
     return 'OK'
 
-def two_state_search(pick_up_state,delivery_state, dollar, minTotalDollar,dist,condition):  
+
+def two_state_search(pick_up_state, delivery_state, dollar, minTotalDollar, dist, condition):
     print("inside the function two way")
     acv = searcher.acv()
-    acv.initDatabase()
     acv.setUp()
     acv.login()
-    acv.two_way(pick_up_state,delivery_state, dollar,minTotalDollar,dist,condition)
+    acv.two_way(pick_up_state, delivery_state, dollar,
+                minTotalDollar, dist, condition)
     return 'OK'
-    
